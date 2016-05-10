76a6a2c0a66541eb521876cd63c5e3fa07e457d1
Olav Nymoen <olav@comoyo.com>
2016-05-11 00:05:16 +0200

    changes

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 let koa = require('koa');
 let serve = require('koa-static');
+let router = require('koa-router')();
 
 let app = koa();
 
-app.use(serve('public'))
+router.get('/search', function *(next) {
+  console.log('SEARCH');
+  console.log(request.query);
+
+  yield next;
+});
+
+router.get('/suggest', function *(next) {
+  console.log('suggest');
+  console.log(request.query);
+
+  this.body = [request.query.q || '', ['cookie']];
+
+  yield next;
+});
+
+app.use(serve('public'));
+
 
 app.listen(process.env.PORT || 8080);
