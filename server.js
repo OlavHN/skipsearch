18ad01fde03db22bcfb960cfe927b2d7706a7a18
Olav Nymoen <olav@comoyo.com>
2016-05-11 00:31:17 +0200

    suggestions working

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 let koa = require('koa');
 let serve = require('koa-static');
 let router = require('koa-router')();
+let request = require('superagent');
 
 let app = koa();
 
 router.get('/search', function *(next) {
-  console.log('SEARCH');
-  console.log(this.request.query);
   this.body = this.request.query.q;
 
   yield next;
 });
 
 router.get('/suggest', function *(next) {
-  // TODO Augment with http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=sea
-  console.log('suggest');
-  console.log(this.request.query);
-
-  this.body = [this.request.query.q || '', ['cookie']];
+  // Delegate suggestions to google
+  this.body = request
+    .get('http://suggestqueries.google.com/complete/search')
+    .query({
+      output: 'firefox',
+      q: this.request.query.q
+    })
 
   yield next;
 });
